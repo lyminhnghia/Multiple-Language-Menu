e76ac2b656677380bd1722e2b619e583a7c9a978
Lý Minh Nghĩa <lmn2461999@gmail.com>
2020-10-07 22:45:17 +0700

    update folder

--- client/src/const/path.const.js
+++ client/src/const/path.const.js
@@ -7,7 +7,9 @@ export const LOGIN_ADMIN = "/admin/login"
 export const SHOP_LIST = "/admin/shop-list"
 export const REGISTER_SHOP = "/admin/register-shop"
 export const ADMIN_PROFILE = "/admin/profile"
+export const ADMIN_NOT_FOUND = "/admin/not-found"
 
 // Shop
 export const LOGIN_SHOP = "/shop/login";
+export const SHOP_NOT_FOUND = "/shop/not-found"
 // Consumer
