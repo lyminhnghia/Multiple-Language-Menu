20dc5e65eb3af1c790559175561aa878218b817d
nghialm99 <nghialm@bglobalcorp.com>
2020-10-11 18:27:28 +0700

    add table shop-list

--- client/src/language/translations/vi.lang.js
+++ client/src/language/translations/vi.lang.js
@@ -2,5 +2,14 @@ export default {
   TXT_LOGIN: "Đăng nhập",
   TXT_SHOP_LIST: "Danh sách cửa hàng",
   TXT_REGISTER_SHOP: "Đăng ký cửa hàng",
-  TXT_PROFILE: "Thông tin cá nhân"
+  TXT_PROFILE: "Thông tin cá nhân",
+  TXT_ID: "ID",
+  TXT_SHOP_NAME: "Tên cửa hàng",
+  TXT_OWNER: "Chủ sở hữu",
+  TXT_ADDRESS: "Địa chỉ",
+  TXT_STAFF_NAME: "Tên nhân viên",
+  TXT_TELEPHONE: "Số điện thoại",
+  TXT_CONTRACT_PERIOD: "Thời hạn hợp đồng",
+  TXT_STATUS: "Trạng thái",
+  TXT_SETTING: "Cài đặt",
 };
--- client/src/pages/Admin/ShopList/index.js
+++ client/src/pages/Admin/ShopList/index.js
@@ -1,42 +1,128 @@
 import React, { memo } from "react";
-import { makeStyles, Paper, Container, Box, TableContainer, Table, TableHead, TableBody, TableRow } from "@material-ui/core"
-import { useTranslation } from "react-i18next"
-import { AdminLayout } from "../../../layouts"
-import { CellBody, CellHead } from "../../../components"
-import { uuid } from "../../../utils"
+import {
+  makeStyles,
+  Box,
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  IconButton,
+} from "@material-ui/core";
+import { MoreHoriz } from "@material-ui/icons";
+import { useTranslation } from "react-i18next";
+import { AdminLayout } from "../../../layouts";
+import { CellBody, CellHead } from "../../../components";
+import { uuid } from "../../../utils";
+import { LangConstant } from "../../../const";
 
-const ShopList = props => {
+const ShopList = (props) => {
   const classes = useStyles();
   const { t: getLabel } = useTranslation();
 
-
   return (
     <AdminLayout>
-      <Paper elevation={1} square className={classes.rootPaper}>
-        <Container>
-          <Box>
-            <TableContainer>
-              <Table>
+      <Box>
+        <TableContainer style={{ marginTop: 50 }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {LangConstant.ARR_SHOP_LIST.map((data, index) => (
+                  <CellHead
+                    cellData={getLabel(data)}
+                    className={classes.cell}
+                    key={uuid()}
+                  />
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {dataShop.data.map((data, index) => (
                 <TableRow>
-                  <CellHead cellData='nghia' className={classes.cell} key={uuid()} />
+                  <CellBody
+                    cellData={data.id}
+                    className={classes.cell}
+                    key={uuid()}
+                  />
+                  <CellBody
+                    cellData={data.shop_name}
+                    className={classes.cell}
+                    key={uuid()}
+                  />
+                  <CellBody
+                    cellData={data.owner}
+                    className={classes.cell}
+                    key={uuid()}
+                  />
+                  <CellBody
+                    cellData={data.address}
+                    className={classes.cell}
+                    key={uuid()}
+                  />
+                  <CellBody
+                    cellData={data.staff_name}
+                    className={classes.cell}
+                    key={uuid()}
+                  />
+                  <CellBody
+                    cellData={data.telephone}
+                    className={classes.cell}
+                    key={uuid()}
+                  />
+                  <CellBody
+                    cellData={data.contract_period}
+                    className={classes.cell}
+                    key={uuid()}
+                  />
+                  <CellBody
+                    cellData={data.status}
+                    className={classes.cell}
+                    key={uuid()}
+                  />
+                  <CellBody
+                    cellData={
+                      <IconButton style={{ padding: 0 }}>
+                        <MoreHoriz />
+                      </IconButton>
+                    }
+                    className={classes.cell}
+                    key={uuid()}
+                  />
                 </TableRow>
-              </Table>
-            </TableContainer>
-          </Box>
-        </Container>
-      </Paper>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
     </AdminLayout>
-  )
+  );
 };
 
-const useStyles = makeStyles(theme => ({
-  rootPaper: {
-    width: "100%",
-    height: "100%"
-  },
+const dataShop = {
+  data: [],
+  page: 1,
+  total: 10,
+};
+
+for (let i = 1; i < 11; i++) {
+  dataShop.data.push({
+    id: i,
+    shop_name: "shop lmn " + i,
+    owner: "Minh Nghia",
+    address: "Lang son",
+    staff_name: "Ngo Thai Son",
+    telephone: "0913098197",
+    contract_period: "Lang Son",
+    status: 1,
+  });
+}
+
+const useStyles = makeStyles((theme) => ({
   cell: {
-    color: "black"
-  }
+    color: "#000000",
+    fontSize: 14,
+    border: "1px solid 	#C0C0C0",
+  },
 }));
 
 export default memo(ShopList);
