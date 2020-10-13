1d18b24acb0a45c9101eb6055e7fa26acad85f59
nghialm99 <nghialm@bglobalcorp.com>
2020-10-14 02:41:28 +0700

    add language

---
+++ server/configs/language.js
@@ -0,0 +1,131 @@
+module.exports = [
+  {
+    lang_code: "zh-cn",
+    lang_name: "Chinese Simplified",
+  },
+  {
+    lang_code: "zh-tw",
+    lang_name: "Chinese Traditional",
+  },
+  {
+    lang_code: "es",
+    lang_name: "Spanish",
+  },
+  {
+    lang_code: "en",
+    lang_name: "English",
+  },
+  {
+    lang_code: "hi",
+    lang_name: "Hindi",
+  },
+  {
+    lang_code: "ar",
+    lang_name: "Arabic",
+  },
+  {
+    lang_code: "pt",
+    lang_name: "Portuguese",
+  },
+  {
+    lang_code: "bn",
+    lang_name: "Bengali",
+  },
+  {
+    lang_code: "ru",
+    lang_name: "Russian",
+  },
+  {
+    lang_code: "ja",
+    lang_name: "Japanese",
+  },
+  {
+    lang_code: "ma",
+    lang_name: "Punjabi",
+  },
+  {
+    lang_code: "de",
+    lang_name: "German",
+  },
+  {
+    lang_code: "jw",
+    lang_name: "Javanese",
+  },
+  {
+    lang_code: "ms",
+    lang_name: "Malay",
+  },
+  {
+    lang_code: "te",
+    lang_name: "Telugu",
+  },
+  {
+    lang_code: "vi",
+    lang_name: "Vietnamese",
+  },
+  {
+    lang_code: "ko",
+    lang_name: "Korean",
+  },
+  {
+    lang_code: "fr",
+    lang_name: "French",
+  },
+  {
+    lang_code: "mr",
+    lang_name: "Marathi",
+  },
+  {
+    lang_code: "ta",
+    lang_name: "Tamil",
+  },
+  {
+    lang_code: "my",
+    lang_name: "Myanmar",
+  },
+  {
+    lang_code: "lo",
+    lang_name: "Lao",
+  },
+  {
+    lang_code: "id",
+    lang_name: "Indonesian",
+  },
+  {
+    lang_code: "ca",
+    lang_name: "Catalan",
+  },
+  {
+    lang_code: "cs",
+    lang_name: "Czech",
+  },
+  {
+    lang_code: "hr",
+    lang_name: "Croatian",
+  },
+  {
+    lang_code: "el",
+    lang_name: "Greek",
+  },
+  {
+    lang_code: "th",
+    lang_name: "Thai",
+  },
+  {
+    lang_code: "ur",
+    lang_name: "Urdu",
+  },
+];
+
+const translate = require("@k3rn31p4nic/google-translate-api");
+
+translate("Thank you", { from: "en", to: "fr" })
+  .then((res) => {
+    console.log(res.text); // OUTPUT: Je vous remercie
+    console.log(res.from.autoCorrected); // OUTPUT: true
+    console.log(res.from.text.value); // OUTPUT: [Thank] you
+    console.log(res.from.text.didYouMean); // OUTPUT: false
+  })
+  .catch((err) => {
+    console.error(err);
+  });
