9b810f4e8cf3418436d404f41d69b624b1684828
nghialm99 <nghialm@bglobalcorp.com>
2020-10-18 14:51:03 +0700

    fix bug admin register

--- client/src/components/inputText.js
+++ client/src/components/inputText.js
@@ -1,37 +1,46 @@
 import React, { memo } from "react";
-import { TextField, makeStyles} from '@material-ui/core';
+import { TextField, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
-    root :{
-        width:"100%"
+  root: {
+    marginTop: 10,
+    width: "100%",
+  },
+  rootTextField: {
+    "& .MuiFormLabel-root": {
+      color: "rgb(48, 92, 139)",
     },
-    rootTextField: {
-        "& .MuiFormLabel-root": {
-          color: "rgb(48, 92, 139)"
-        },
-        "& .MuiInputBase-root": {
-            color: "#000000"
-        }
+    "& .MuiInputBase-root": {
+      color: "#000000",
     },
-    formControl: {
-        display: "flex"
-    },
+  },
+  formControl: {
+    display: "flex",
+  },
 });
 
-const InputText = ({nameLabel,typeInput,requiredInput,defaultValueInput, onInput, nameText,disabled}) => {
+const InputText = ({
+  nameLabel,
+  typeInput,
+  requiredInput,
+  defaultValueInput,
+  onInput,
+  nameText,
+  disabled,
+}) => {
   const classes = useStyles();
   return (
-    <TextField 
-        className = {`${classes.formControl} ${classes.rootTextField}`} 
-        classes = {{root:classes.root}} 
-        id="standard-basic" 
-        label = {nameLabel}
-        type = {typeInput}
-        required = {requiredInput}
-        defaultValue = {defaultValueInput}
-        onInput = {onInput}
-        name={nameText}
-        disabled={disabled}
+    <TextField
+      className={`${classes.formControl} ${classes.rootTextField}`}
+      classes={{ root: classes.root }}
+      id="standard-basic"
+      label={nameLabel}
+      type={typeInput}
+      required={requiredInput}
+      defaultValue={defaultValueInput}
+      onInput={onInput}
+      name={nameText}
+      disabled={disabled}
     />
   );
 };
